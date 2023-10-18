aeeaa6eabc4adda5001c5d1a73cd446ab289f9d0
mynewyear <natalia.iakovleva@hotmail.com>
2023-10-17 21:39:44 -0700

    Handle AddTodoForm, and store new task

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddTodoForm = () => {
-    return (
-        <div>
-            <form>
-                <label htmlFor="todoTitle">Title:</label>
-                <br />
-                <input type="text" id="todoTitle" required />
-                <br />
-                <button type="submit">Add</button>
-            </form>
-        </div>
-    );
+const AddTodoForm = ({ onAddTodo }) => { // Accept onAddTodo as a prop
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    onAddTodo(todoTitle); // Call onAddTodo and pass todoTitle
+    setTodoTitle("");
+  };
+
+  const handleChange = (event) => {
+    setTodoTitle(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleAddTodo}>
+        <label htmlFor="todoTitle">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="todoTitle"
+          name="title"
+          required
+          value={todoTitle}
+          onChange={handleChange}
+        />
+        <br />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddTodoForm;

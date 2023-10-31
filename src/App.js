91632d03439b40c6237b8a89bc48b72ddc5e406f
mynewyear <natalia.iakovleva@hotmail.com>
2023-10-31 14:48:35 -0700

    lesson 1_4, Props Handling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  const [newTodo, setNewTodo] = useState(""); // state for newTodo
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(newTodo) {
+    setTodoList([...todoList, newTodo]);
+  }
 
   return (
-    <>
+    <div>
       <h1>ToDo List</h1>
-      <AddTodoForm onAddTodo={setNewTodo} /> {/* pass onAddTodo */}
-      <p>New task: {newTodo}</p> {/* display newTodo */}
-      <TodoList />
-    </>
+      <AddTodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
+    </div>
   );
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,35 +1,18 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
-
-const todoList = [
-    {
-      id: 1,
-      title: "Wake up",
-    },
-    {
-      id: 2,
-      title: "Take a shower",
-    },
-    {
-      id: 3,
-      title: "Take a coffee",
-    },
-    {
-      id: 4,
-      title: "Learn React",
-    },
-  ];
   
-const TodoList = () => {
-return (
+function TodoList({ todoList }) {
+  return (
     <>
-    <ul>
-        {todoList.map((item) => (
-          <TodoListItem key={item.id} todo={item} />
-        ))}
-    </ul>
+      <ul>
+        {todoList.map(function (item) {
+          return (
+            <TodoListItem key={item.id} todo={item} />
+          );
+        })}
+      </ul>
     </>
-);
-};
+  );
+}
 
 export default TodoList;
